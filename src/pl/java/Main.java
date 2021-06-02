91cb77f45ce79ac2df0c12b4f58b9ec4921c2967
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-06-02 09:46:43 +0200

    Updated Main file

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -3,5 +3,6 @@ package pl.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test project for GitHub repository");
+        System.out.println("Hello World from project's clone");
     }
 }
